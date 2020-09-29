d012194606232e7a2ad74c6e183eb3d62472e0a2
jsg3121 <xodm95@gamil.com>
2020-09-29 20:26:18 +0900

    axios 추가 및 백엔드 연결

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
 
-Vue.config.productionTip = false
+// 추가된 부분
+import axios from "axios";
+Vue.prototype.$http = axios;
+// this.$http를 통해 get, post같은 http request를 가능하게 함
+// 추가된 부분 끝
+Vue.config.productionTip = false;
 
+/* eslint-disable no-new */
 new Vue({
+  el: "#app",
   router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+  components: { App },
+  template: "<App/>"
+});
