6c12ae70463809c848de3fc0232e1410b389502a
jsg3121 <xodm95@gamil.com>
2020-09-29 20:55:07 +0900

    화면에 게시글 표시하기 완료

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import Posting from "@/views/Posting";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "Posting",
+      component: Posting
     }
   ]
-})
+});
---
+++ frontend/src/views/Posting.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="posting-container">
+    <h1>게시판</h1>
+    <div class="inner-container">
+      <table class="posting">
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>제목</th>
+            <th>작성자</th>
+            <th>날짜</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="list in posting" :key="list.data_id">
+            <td>{{ list.data_id }}</td>
+            <td>{{ list.title }}</td>
+            <td>{{ list.user_id }}</td>
+            <td>{{ list.upload_date }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.$http.get("/posting").then((response) => {
+      this.posting = response.data;
+    });
+  },
+  data() {
+    return {
+      posting: [],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
